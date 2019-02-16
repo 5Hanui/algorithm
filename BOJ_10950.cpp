f503a2a28b0c97eb267882b3fa086de74ecec3c8
오하늬 <gkdud644@naver.com>
2019-02-16 16:23:51 +0900

    10950번 : A + B 구하기

---
+++ BOJ_10950.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int testCase, a, b;
+	cin >> testCase;
+	while (testCase--) {
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+	return 0;
+}
